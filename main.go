ca8feaf55d270807c659cc2b72d2444d99b54557
Rizya Rafael <rizyarafaelbro@gmail.com>
2024-12-17 18:26:56 +0700

    fix: env for port

--- main.go
+++ main.go
@@ -27,9 +27,12 @@ func main() {
 	DB_USER := os.Getenv("DB_USER")
 	DB_PASS := os.Getenv("DB_PASS")
 	DB_NAME := os.Getenv("DB_NAME")
+	DB_PORT := os.Getenv("DB_PORT")
+	
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=5432 sslmode=require", DB_HOST, DB_USER, DB_PASS, DB_NAME)
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=require", DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	log.Print(db)
 
 	if err != nil {
 		log.Fatal(err)
